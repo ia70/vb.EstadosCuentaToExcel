8e471ebe60e7d09535c9018096d166a9f212a12d
xcoru <pcd510@hotmail.com>
2020-11-05 04:26:10 -0600

    Cambios

--- EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo_Banamex.vb
+++ EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo_Banamex.vb
@@ -4,6 +4,9 @@ Imports Capa_Identidad
 Public Class N_Algoritmo_Banamex
     Inherits N_Algoritmo
 
+    Private elementos, no_decimales, no_cadenas, no_fechas As Integer
+    Private size_fecha As Integer
+
     Public Sub New()
     End Sub
 
@@ -16,6 +19,7 @@ Public Class N_Algoritmo_Banamex
         Dim aux As String
         Dim indice As Integer
 
+        CargarVariables()
         aux = GetLinea(cadena)
         Do
             Try
@@ -48,6 +52,8 @@ Public Class N_Algoritmo_Banamex
                 size += .Fecha_operacion_separador_dia_mes.Length
             End If
         End With
+
+        size_fecha = size
 
         in1 = GetFechaIndice(cadena, size)
         If in1 >= 0 Then
@@ -166,12 +172,134 @@ Public Class N_Algoritmo_Banamex
 
     End Function
 
+    Private Sub CargarVariables()
+        Try
+            elementos = _formato.Formato_campos.Count
+
+            For Each campo As I_Formato_campos In _formato.Formato_campos
+                Select Case campo.Tipo.ToUpper
+                    Case "DATETIME"
+                        no_fechas += 1
+                    Case "STRING"
+                        no_cadenas += 1
+                    Case "DECIMAL"
+                        no_decimales += 1
+                End Select
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
     Private Function ProcesarLinea(ByVal cadena As String) As List(Of String)
+        Dim numeros As New List(Of String)
+        Dim fechas As New List(Of String)
+        Dim numero, copy, aux As String
+        Dim i, indice As Integer
 
+        copy = cadena
+        'OBTENER CIFRAS -----------------------------------------------
+        Try
+            Do
+                numero = GetCantidad(cadena)
+                If numero.Length > 0 Then
+                    numeros.Add(numero)
+                    indice = cadena.IndexOf(numero)
+                    cadena = cadena.Remove(indice, numero.Length)
+                    i += 1
+                Else
+                    indice = cadena.IndexOf(".")
+                    If indice >= 0 Then
+                        cadena = cadena.Substring(indice + 1)
+                    Else
+                        Exit Do
+                    End If
+                End If
+            Loop While cadena.Length > 0
+        Catch ex As Exception
 
+        End Try
+        cadena = copy
 
+        'QUITAR CIFRAS DE CADENA --------------------------------------
+        For Each linea As String In numeros
+            indice = cadena.IndexOf(linea)
+            If indice >= 0 Then
+                cadena = cadena.Remove(indice, linea.Length)
+            End If
+        Next
 
+        'QUITAR FECHA -------------------------------------------------
+        For i = 0 To no_fechas - 1
+            aux = cadena.Substring(0, size_fecha)
+            fechas.Add(aux)
+            cadena = cadena.Substring(aux.Length + 1)
+        Next
+
+        'ELIMINA SALTOS DE LINEA Y ESPACIOS EN BLANCOS DUPLICADOS
+        cadena = cadena.Replace(vbLf, " ")
+        cadena = cadena.Replace("   ", " ")
+        cadena = cadena.Replace("  ", " ")
+
+        'MANEJAR LA INFORMACION
+
+        Return numeros
     End Function
 
+    Private Function GetCantidad(ByVal cadena As String) As String
+        Dim copy As String
+        Dim indice, ini, fin, i, size As Integer
+        Dim numero As String
+
+        Try
+            copy = cadena
+            size = cadena.Length
+            indice = cadena.IndexOf(".")
+
+            Do
+                If indice >= 0 Then
+                    i = indice - 1
+                    While (i >= 0 Or Not cadena(i) = " ") And (IsNumeric(cadena(i)) Or cadena(i) = ",")
+                        i -= 1
+                    End While
+                    If i >= 0 And (IsNumeric(cadena(i + 1)) Or cadena(i + 1) = ",") Then
+                        ini = i + 1
+                        If IsNumeric(cadena(indice + 1)) And IsNumeric(cadena(indice + 2)) Then
+                            If ((indice + 3) <= size - 1) Then
+                                If cadena(indice + 3) = " " Or cadena(indice + 3) = vbLf Then
+                                    fin = indice + 3
+                                    Exit Do
+                                Else
+                                    Return ""
+                                End If
+                            Else
+                                fin = indice + 3
+                                Exit Do
+                            End If
+                        Else
+                            Return ""
+                        End If
+                    Else
+                        Return ""
+                    End If
+                Else
+                    Return ""
+                End If
+
+                cadena = cadena.Substring(indice + 1)
+                size = cadena.Length
+                indice = cadena.IndexOf(".")
+            Loop While indice >= 0
+
+            cadena = cadena.Substring(ini, fin - ini)
+
+            numero = cadena
+
+            Return numero
+        Catch ex As Exception
+            Return ""
+        End Try
+
+    End Function
 
 End Class
