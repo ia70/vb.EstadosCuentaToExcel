6edab4687cb024f43cf79d73b56728ad06f8c396
xcoru <pcd510@hotmail.com>
2020-11-04 21:10:22 -0600

    Cambios

--- EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo.vb
+++ EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo.vb
@@ -2,8 +2,10 @@
 Public MustInherit Class N_Algoritmo
     Protected _cadena As String
     Protected _archivo As I_Archivo
-    Protected _formato As I_formato
+    Protected _formato As I_Formato
+    Protected _mesesAbr As New List(Of String)
 
+#Region "CONSTRUCTOR/PROPIEDAD"
     Public Sub New()
     End Sub
 
@@ -12,7 +14,7 @@ Public MustInherit Class N_Algoritmo
     ''' </summary>
     ''' <param name="cadena">PDF en texto plano</param>
     ''' <param name="formato">Identidad del formato</param>
-    Public Sub New(ByVal cadena As String, ByVal formato As I_formato)
+    Public Sub New(ByVal cadena As String, ByVal formato As I_Formato)
         _cadena = cadena
         _archivo = New I_Archivo(formato.Formato_campos)
         _formato = formato
@@ -33,7 +35,10 @@ Public MustInherit Class N_Algoritmo
         End Set
     End Property
 
+#End Region
 #Region "FUNCIONES"
+#Region "VARIAS"
+
     Protected Sub ProcesarInfo()
         Try
             _cadena = LimpiarTexto(_cadena)
@@ -44,8 +49,109 @@ Public MustInherit Class N_Algoritmo
     End Sub
 
     Protected MustOverride Function GetLinea(ByVal cadena As String) As String
-
     Protected MustOverride Sub GetDatos()
+
+    ''' <summary>
+    ''' Se encarga de quitar todo lo que no es el cuerpo del estado de cuenta
+    ''' </summary>
+    ''' <param name="cadena">cadena a limpiar</param>
+    ''' <returns></returns>
+    Protected Function LimpiarTexto(ByVal cadena As String) As String
+        Dim ig_ini, ig_fin, ig_total_ini, ig_total_fin As String
+        Dim aux, aux2 As String
+        Dim indice As Integer
+
+        'CARGAR VARIABLES
+        ig_ini = _formato.Formato_global.Ignora_parcial_ini
+        ig_fin = _formato.Formato_global.Ignora_parcial_fin
+        ig_total_ini = _formato.Formato_global.Detalles_saldo_ini
+        ig_total_fin = _formato.Formato_global.Detalles_saldo_fin
+
+        Try
+            'RECUPERAR DATOS GLOBALES
+            With _archivo
+                .Rfc = GetRFC()
+                .Fecha = GetFecha()
+                .Saldo_inicial = GetSaldoInicial()
+                .No_cuenta = GetNoCuenta()
+            End With
+
+            _mesesAbr.Add(GetMesAbreviacion(_archivo.Fecha.Month - 1))
+            _mesesAbr.Add(GetMesAbreviacion(_archivo.Fecha.Month))
+            _mesesAbr.Add(GetMesAbreviacion(_archivo.Fecha.Month + 1))
+
+        Catch ex As Exception
+        End Try
+
+        Try
+            'QUITAR ENCABEZADO
+            indice = cadena.IndexOf(ig_total_ini)
+            cadena = cadena.Substring(indice + ig_total_ini.Length)
+
+            'QUITAR PIE
+            indice = cadena.IndexOf(ig_total_fin)
+            cadena = cadena.Substring(0, indice)
+
+            'QUITAR PARCIAL
+            While cadena.IndexOf(ig_ini) >= 0
+                indice = cadena.IndexOf(ig_ini)
+                'indice2 = cadena.IndexOf(ig_fin)
+
+                aux = cadena.Substring(0, indice)
+                indice = cadena.IndexOf(ig_fin)
+
+                aux2 = cadena.Substring(indice + ig_fin.Length)
+                cadena = aux + aux2
+            End While
+
+        Catch ex As Exception
+        End Try
+
+        Return cadena
+    End Function
+
+    ''' <summary>
+    ''' Se encarga de extraer la información de un campo
+    ''' </summary>
+    ''' <param name="cad_ini">Prefijo de inicio</param>
+    ''' <param name="cad_fin">Prefijo de final</param>
+    ''' <returns></returns>
+    Protected Function GetCampo(ByVal cad_ini As String, cad_fin As String) As String
+        Return GetCampo(_cadena, cad_ini, cad_fin)
+    End Function
+
+    ''' <summary>
+    ''' Se encarga de extraer la información de un campo
+    ''' </summary>
+    ''' <param name="vcadena">Cadena en la cual buscar</param>
+    ''' <param name="cad_ini">Prefijo de inicio</param>
+    ''' <param name="cad_fin">Prefijo de final</param>
+    ''' <returns></returns>
+    Protected Function GetCampo(ByVal vcadena As String, ByVal cad_ini As String, cad_fin As String) As String
+        Dim cadenaAux As String
+        Dim ini, fin As Integer
+        cadenaAux = vcadena
+
+        Try
+            ini = cadenaAux.IndexOf(cad_ini)
+            If ini >= 0 Then
+                cadenaAux = cadenaAux.Substring(ini + cad_ini.Length)
+            End If
+
+            fin = cadenaAux.IndexOf(cad_fin)
+            If fin >= 0 Then
+                cadenaAux = cadenaAux.Substring(0, fin)
+            End If
+
+            Return cadenaAux
+        Catch ex As Exception
+            Return ""
+        End Try
+
+    End Function
+
+#End Region
+#Region "OBTENER CAMPOS"
 
     Protected Function GetRFC() As String
         Return GetCampo(_formato.Formato_global.Rfc_ini, _formato.Formato_global.Rfc_fin)
@@ -118,59 +224,8 @@ Public MustInherit Class N_Algoritmo
         Return noCuenta
     End Function
 
-    ''' <summary>
-    ''' Se encarga de quitar todo lo que no es el cuerpo del estado de cuenta
-    ''' </summary>
-    ''' <param name="cadena">cadena a limpiar</param>
-    ''' <returns></returns>
-    Protected Function LimpiarTexto(ByVal cadena As String) As String
-        Dim ig_ini, ig_fin, ig_total_ini, ig_total_fin As String
-        Dim aux, aux2 As String
-        Dim indice As Integer
-
-        'CARGAR VARIABLES
-        ig_ini = _formato.Formato_global.Ignora_parcial_ini
-        ig_fin = _formato.Formato_global.Ignora_parcial_fin
-        ig_total_ini = _formato.Formato_global.Detalles_saldo_ini
-        ig_total_fin = _formato.Formato_global.Detalles_saldo_fin
-
-        Try
-            'RECUPERAR DATOS GLOBALES
-            With _archivo
-                .Rfc = GetRFC()
-                .Fecha = GetFecha()
-                .Saldo_inicial = GetSaldoInicial()
-                .No_cuenta = GetNoCuenta()
-            End With
-        Catch ex As Exception
-        End Try
-
-        Try
-            'QUITAR ENCABEZADO
-            indice = cadena.IndexOf(ig_total_ini)
-            cadena = cadena.Substring(indice + ig_total_ini.Length)
-
-            'QUITAR PIE
-            indice = cadena.IndexOf(ig_total_fin)
-            cadena = cadena.Substring(0, indice)
-
-            'QUITAR PARCIAL
-            While cadena.IndexOf(ig_ini) >= 0
-                indice = cadena.IndexOf(ig_ini)
-                'indice2 = cadena.IndexOf(ig_fin)
-
-                aux = cadena.Substring(0, indice)
-                indice = cadena.IndexOf(ig_fin)
-
-                aux2 = cadena.Substring(indice + ig_fin.Length)
-                cadena = aux + aux2
-            End While
-
-        Catch ex As Exception
-        End Try
-
-        Return cadena
-    End Function
+#End Region
+#Region "OPERACIONES CON FECHAS"
 
     ''' <summary>
     ''' Devuelme el numero correspondiente al mes dado
@@ -267,43 +322,47 @@ Public MustInherit Class N_Algoritmo
     End Function
 
     ''' <summary>
-    ''' Se encarga de extraer la información de un campo
+    ''' Obtener la abreviación del mes
     ''' </summary>
-    ''' <param name="cad_ini">Prefijo de inicio</param>
-    ''' <param name="cad_fin">Prefijo de final</param>
+    ''' <param name="num">Número del mes</param>
     ''' <returns></returns>
-    Protected Function GetCampo(ByVal cad_ini As String, cad_fin As String) As String
-        Return GetCampo(_cadena, cad_ini, cad_fin)
+    Protected Function GetMesAbreviacion(ByVal num As Integer) As String
+        Dim mes As String
+
+        Select Case num
+            Case 0
+                mes = "DIC"
+            Case 1
+                mes = "ENE"
+            Case 2
+                mes = "FEB"
+            Case 3
+                mes = "MAR"
+            Case 4
+                mes = "ABR"
+            Case 5
+                mes = "MAY"
+            Case 6
+                mes = "JUN"
+            Case 7
+                mes = "JUL"
+            Case 8
+                mes = "AGO"
+            Case 9
+                mes = "SEP"
+            Case 10
+                mes = "OCT"
+            Case 11
+                mes = "NOV"
+            Case 12
+                mes = "DIC"
+            Case Else
+                mes = "ENE"
+        End Select
+
+        Return mes
     End Function
 
-    ''' <summary>
-    ''' Se encarga de extraer la información de un campo
-    ''' </summary>
-    ''' <param name="vcadena">Cadena en la cual buscar</param>
-    ''' <param name="cad_ini">Prefijo de inicio</param>
-    ''' <param name="cad_fin">Prefijo de final</param>
-    ''' <returns></returns>
-    Protected Function GetCampo(ByVal vcadena As String, ByVal cad_ini As String, cad_fin As String) As String
-        Dim cadenaAux As String
-        Dim ini, fin As Integer
-        cadenaAux = vcadena
-
-        Try
-            ini = cadenaAux.IndexOf(cad_ini)
-            If ini >= 0 Then
-                cadenaAux = cadenaAux.Substring(ini + cad_ini.Length)
-            End If
-
-            fin = cadenaAux.IndexOf(cad_fin)
-            If fin >= 0 Then
-                cadenaAux = cadenaAux.Substring(0, fin)
-            End If
-
-            Return cadenaAux
-        Catch ex As Exception
-            Return ""
-        End Try
-
-    End Function
+#End Region
 #End Region
 End Class
--- EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo_Banamex.vb
+++ EstadosCuentaToExcel/Capa_Negocios/N_Algoritmo_Banamex.vb
@@ -33,6 +33,113 @@ Public Class N_Algoritmo_Banamex
     End Sub
 
     Protected Overrides Function GetLinea(cadena As String) As String
+        Dim separador, fecha As String
+        Dim sep_tipo As Integer
+        Dim size As Integer
+        Dim in1, in2 As Integer
+
+
+
+        With _formato.Formato_global
+
+            size = .Fecha_operacion_dia_length + .Fecha_operacion_mes_length + .Fecha_operacion_anio_length
+            If .Fecha_operacion_mes_length > 0 Then
+                size += .Fecha_operacion_separador_dia_mes.Length
+            End If
+            If .Fecha_operacion_anio_length > 0 Then
+                size += .Fecha_operacion_separador_dia_mes.Length
+            End If
+
+
+
+        End With
+
+        separador = _formato.Formato_global.Fecha_operacion_separador_dia_mes
+
+        For Each mes As String In _mesesAbr
+            'in1 = cadena.IndexOf()
+        Next
+
+
+
+    End Function
+
+    Private Function VerificarFecha(ByVal cadena As String) As Boolean
+        Dim aux, copy As String
+        Dim veri As Boolean
+        copy = cadena
+
+
+        With _formato.Formato_global
+
+            If .Fecha_operacion_dia_length > 0 Then
+                aux = cadena.Substring(0, .Fecha_operacion_dia_length)
+                If IsNumeric(aux) Then
+                    cadena = cadena.Substring(.Fecha_operacion_dia_length)
+                    aux = cadena.Substring(0, .Fecha_operacion_separador_dia_mes.Length)
+                    If aux = .Fecha_operacion_separador_dia_mes Then
+                        If .Fecha_operacion_mes_length > 0 Then
+                            cadena = cadena.Substring(.Fecha_operacion_separador_dia_mes.Length)
+                            aux = cadena.Substring(0, .Fecha_operacion_mes_length)
+
+                            Select Case .Fecha_operacion_mes_length
+                                Case 2
+                                    veri = IsNumeric(aux)
+                                Case 3
+                                    veri = GetMesNombre(aux).Length > 3
+                                Case >= 4
+                                    veri = GetMesNum(aux).Length > 0
+                            End Select
+
+                            If veri Then
+                                If .Fecha_liquidacion_anio_length > 0 Then
+                                    cadena = cadena.Substring(.Fecha_operacion_mes_length)
+                                    aux = cadena.Substring(0, .Fecha_operacion_separador_dia_mes.Length)
+
+                                    If aux = .Fecha_operacion_separador_dia_mes Then
+                                        cadena = cadena.Substring(.Fecha_operacion_separador_dia_mes.Length)
+                                        aux = cadena.Substring(0, .Fecha_operacion_anio_length)
+
+                                        If IsNumeric(aux) Then
+                                            cadena = cadena.Substring(.Fecha_operacion_anio_length)
+                                            If cadena.Length = 0 Then
+                                                Return True
+                                            Else
+                                                Return False
+                                            End If
+                                        Else
+                                                Return False
+                                        End If
+
+                                    Else
+                                        Return False
+                                    End If
+
+                                Else
+                                    cadena = cadena.Substring(.Fecha_operacion_mes_length)
+                                    If cadena.Length = 0 Then
+                                        Return True
+                                    Else
+                                        Return False
+                                    End If
+                                End If
+                            Else
+                                Return False
+                            End If
+
+                        Else
+                            Return True
+                        End If
+                    Else
+                        Return False
+                    End If
+                Else
+                    Return False
+                End If
+            End If
+
+        End With
+
 
     End Function
 
